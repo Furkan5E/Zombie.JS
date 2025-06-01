70612d660b6df5b2b073d0a6d6ba796c0d3e3cd5
Furkan5E <furkankeles10@hotmail.com>
2025-06-01 17:16:38 +0100

    Feat(gameHandler.js): implemented pause game logic

--- js/classes/gameHandler.js
+++ js/classes/gameHandler.js
@@ -9,6 +9,9 @@ class GameHandler {
         this.controls = new Controls(this);
         this.gamePlaying = new GamePlaying(this);
 
+        this.paused = false;
+        this.pauseCooldown = 0;
+
         this.chosenState = this.menu;
 
         this.bulletsArray = [];
@@ -19,11 +22,42 @@ class GameHandler {
         this.spawnCooldown = 0;
     }
 
-    draw(){
+    draw() {
+    if (this.chosenState === this.gamePlaying) {
+        if (this.pauseCooldown > 0) {
+            this.pauseCooldown--;
+        }
+        // toggle pause
+        if (keyIsDown(80) && this.pauseCooldown === 0){
+            this.pauseCooldown = 17;
+            this.paused = !this.paused;
+        }
+
+        if (this.paused) {
+            this.pause(); //pause game
+            return;
+        }
+
         this.chosenState.draw();
+    }
+    else{
+        this.paused == false;
+        this.chosenState.draw();
+    }
+}
+
+    pause(){
+        push();
+        textAlign(CENTER, CENTER);
+        textSize(64);
+        text("PAUSED", width /2, height/2 - 25);
+        pop();
     }
 
     mousePressed() {
+        if (this.paused){
+            return;
+        }
         this.chosenState.mousePressed();
     }
 }
