1238144d0046d6d8b2b930aa4d12b931d00e27e8
Furkan5E <furkankeles10@hotmail.com>
2025-06-12 18:19:36 +0100

    Feat(config.js): moved asset loading to config

--- js/config.js
+++ js/config.js
@@ -1,3 +1,11 @@
+//constants
+const KEYS = {
+    PAUSE: 80,   // P
+    RESTART: 82, // R
+    MENU: 27     // Esc
+};
+
+//asset handling
 let heartImg;
 let playerImg;
 let playerHurtImg;
@@ -16,3 +24,25 @@ let zombieHurtSound;
 let splitSound;
 let selectSound;
 let backgroundMusic;
+
+function preload() { // load assets
+  font = loadFont('assets/font.otf');
+
+  heartImg = loadImage("assets/heart.png");
+  playerImg = loadImage("assets/player.png");
+  playerHurtImg = loadImage("assets/playerHurt.png");
+  zombieImg = loadImage("assets/zombie.png");
+  zombieHurtImg = loadImage("assets/zombieHurt.png");
+  fastZombieImg = loadImage("assets/fastZombie.png");
+  strongZombieImg = loadImage("assets/strongZombie.png");
+  bossZombieImg = loadImage("assets/bossZombie.png");
+  minionZombieImg = loadImage("assets/minionZombie.png");
+  splitZombieImg = loadImage("assets/splitZombie.png");
+
+  playerHurtSound = loadSound("sounds/playerHurt.wav");
+  shootSound = loadSound("sounds/shoot.wav");
+  zombieHurtSound = loadSound("sounds/zombieHurt.wav");
+  splitSound = loadSound("sounds/split.wav");
+  selectSound = loadSound("sounds/select.wav");
+  backgroundMusic = loadSound("sounds/battleship.ogg")
+}
--- js/sketch.js
+++ js/sketch.js
@@ -1,30 +1,8 @@
 let game = new GameHandler();
 
-function preload() { // load assets
-  font = loadFont('assets/font.otf');
-
-  heartImg = loadImage("assets/heart.png");
-  playerImg = loadImage("assets/player.png");
-  playerHurtImg = loadImage("assets/playerHurt.png");
-  zombieImg = loadImage("assets/zombie.png");
-  zombieHurtImg = loadImage("assets/zombieHurt.png");
-  fastZombieImg = loadImage("assets/fastZombie.png");
-  strongZombieImg = loadImage("assets/strongZombie.png");
-  bossZombieImg = loadImage("assets/bossZombie.png");
-  minionZombieImg = loadImage("assets/minionZombie.png");
-  splitZombieImg = loadImage("assets/splitZombie.png");
-
-  playerHurtSound = loadSound("sounds/playerHurt.wav");
-  shootSound = loadSound("sounds/shoot.wav");
-  zombieHurtSound = loadSound("sounds/zombieHurt.wav");
-  splitSound = loadSound("sounds/split.wav");
-  selectSound = loadSound("sounds/select.wav");
-  backgroundMusic = loadSound("sounds/battleship.ogg")
-}
-
 function setup() {
   createCanvas(900, 600);
-  backgroundMusic.loop();
+  game.states.SETTINGS.playMusic(backgroundMusic)
 }
 
 function draw() {
