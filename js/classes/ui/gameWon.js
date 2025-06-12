3a1ffc2132d66039b825bc95fc3b12afe0b31061
Furkan5E <furkankeles10@hotmail.com>
2025-06-12 18:22:17 +0100

    Feat(gameWon.js): class updated to use new UI system

--- js/classes/ui/gameWon.js
+++ js/classes/ui/gameWon.js
@@ -1,36 +1,32 @@
 class GameWon {
     constructor(game){
         this.game = game;
+
+         this.text = [
+            new Text(450, 180, 100, "YOU WON!"),
+            new Text(450, 300, 50, "You defeated the zombie horde!"),
+            new Text(450, 360, 50, "Well done!"),
+            new Text(450, 420, 45, "Press R to play again"),
+            new Text(450, 480, 40, "Press ESC to return to Main Menu")
+        ];
     }
 
     draw() {
         background("black");
-        textFont(font);
-        textSize(100);
-        fill(255,255,255);
-        textAlign(CENTER);
 
-        textSize(100);
-        text("YOU WON!", 450, 200);
+        for (let i = 0; i < this.text.length; i++) {
+            this.text[i].draw();
+        }
 
-        textSize(50);
-        text("You defeated the zombie horde!", 450, 260);
-        text("Well done!", 450, 320);
-
-        textSize(45);
-        text("Press R to to play again", 450, 380);
-
-        textSize(40);
-        text("Return to Main Menu", 450, 500);
-        
-        if (keyIsDown(82))  { // R key
-            this.game.gamePlaying.startGame();
+        if (keyIsDown(KEYS.RESTART)) {
+            this.game.states.PLAYING.startGame();
+        }
+        else if (keyIsDown(KEYS.MENU)){
+            this.game.setState("MENU");
         }
     }
+
     mousePressed(){
-        if (mouseX <= 655 && mouseX >= 235 && mouseY <= 503 && mouseY >= 465) { // return to menu button position
-            this.game.chosenState = this.game.menu;
-            selectSound.play();
-        }
+
     }
 }
